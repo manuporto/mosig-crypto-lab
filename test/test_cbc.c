2023ead37cb33825340c50af0264da4edb986bfd
Manuel Porto <manu.porto94@gmail.com>
2019-04-26 10:36:45 +0200

    Update tests

--- test/test_cbc.c
+++ test/test_cbc.c
@@ -1,54 +1,66 @@
 #include "../src/cbc.h"
 #include "test_cbc.h"
 
+#include <assert.h>
+#include <stdbool.h> 
 #include <stdio.h>
-
-uint64_t first_iv = 0;
-uint64_t second_iv = 0;
 
 void test_cbc_enc_is_not_deterministic() {
     size_t plen = 8;
-    uint8_t pt[] = {0, 0, 0, 0, 0, 0, 0, 0};
-    uint8_t ct_1[plen];
-    uint8_t ct_2[plen];
-    uint64_t key[] = {0, 0};
+    uint8_t pt[8] = {0};
+    // size_t plen = 32;
+    // uint8_t pt[] = "0123456789abcdef0123456789abcdef";
+    size_t clen = 24;
+    // size_t clen = 48;
+    uint8_t ct_1[24] = {0};
+    uint8_t ct_2[24] = {0};
+    uint8_t ct_3[24] = {0}; // If we remove this we get a stack smashed error, no idea why
+    uint64_t key[2] = {0};
     printf("Pringint plain text...\n");
     for (size_t i = 0; i < plen; i++) {
-        printf("x%lu: %u\n", i, pt[i]);
+        printf("x%lu: %u ", i, pt[i]);
     }
-    printf("Done\n");
+    printf("\nDone\n");
     cbc_enc(key, pt, ct_1, plen);
     cbc_enc(key, pt, ct_2, plen);
 
     printf("Printing cipher texts...\n");
-    for (size_t i = 0; i < plen; i++) {
+    bool equals = true;
+    for (size_t i = 0; i < clen; i++) {
         printf("%lu: %u | %u\n", i, ct_1[i], ct_2[i]);
-        assert(ct_1[i] != ct_2[i]);
+        if (ct_1[i] != ct_2[i]) {
+          equals = false;
+          break;
+        }
     }
+    assert(!equals);
     printf("Done\n");
 }
 
 void test_cbc_enc_and_dec() {
-  uint64_t key[2] = {0, 0};
-  size_t plen = 32;
-  uint8_t plaintext[32] = "0123456789abcdef0123456789abcdef";
-  uint8_t ciphertext[32] = {};
-  uint8_t plaintext2[32] = {};
+uint64_t key[2] = {0, 0};
+    //
+    size_t plen = 32;
+    uint8_t plaintext[] = "0123456789abcdef0123456789abcdef";
+    // uint8_t plaintext[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    // Cipher text has 16 additional bytes to store the IV
+    size_t clen = 48;
+    uint8_t ciphertext[48] = {};
+    uint8_t plaintext2[32] = {};
 
-  cbc_enc(key, plaintext, ciphertext, plen);
+    cbc_enc(key, plaintext, ciphertext, plen);
 
-  printf("Encryption:\n");
-  for (size_t i = 0; i < plen; i++) {
-    printf("%u ", ciphertext[i]);
-  }
-  first_iv = 0;
-  second_iv = 0;
-  cbc_dec(key, ciphertext, plaintext2, plen);
-  printf("\nDecryption:\n");
-  for (size_t i = 0; i < plen; i++) {
-    printf("%c", plaintext2[i]);
-    assert(plaintext[i] == plaintext2[i]);
-  }
+    printf("Encryption (IV not included):\n");
+    for (size_t i = 16; i < clen; i++) {
+      printf("%u ", ciphertext[i]);
+    }
+
+    cbc_dec(key, ciphertext, plaintext2, plen);
+    printf("\nDecryption:\n");
+    for (size_t i = 0; i < plen; i++) {
+      printf("%c", plaintext2[i]);
+      assert(plaintext[i] == plaintext2[i]);
+    }
 }
 
 void run_cbc_enc_tests() {
